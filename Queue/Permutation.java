4e050c706db2e539dcff9ec40bcac7093d61c531
Baiyu Zhao <cs@baiyu.me>
2017-01-05 16:26:15 +0800

    small error

--- Queue/Permutation.java
+++ Queue/Permutation.java
@@ -5,14 +5,9 @@ public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> rq = new RandomizedQueue<String>();
-        for (int i = 0; i < k; ++i) {
-            rq.enqueue(StdIn.readString());
-        }
-        /*
         while (!StdIn.isEmpty()) {
             rq.enqueue(StdIn.readString());
         }
-        */
         for (int i = 0; i < k; ++i) {
             StdOut.println(rq.dequeue());
         }
